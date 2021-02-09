b641e2790b0585154126d048903a0721481c4ea3
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2021-02-09 18:32:44 +0900

    ルーター追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "./App.css";
 import PiePlot from "./components/PiePlot";
+import Main from "./components/Main";
 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <PiePlot />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/pieprot" component={PiePlot} exact />
+        <Route path="/" component={Main} exact />
+      </Switch>
+    </Router>
   );
 };
 
