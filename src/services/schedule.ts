a6a9227579d853efd638572a5bc4f15da3b0df14
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2021-02-10 17:33:40 +0900

    収入追加

---
+++ src/services/schedule.ts
@@ -0,0 +1,24 @@
+import { isSameDay } from "./calendar";
+import { CalendarType } from "../components/CalendarBoard/index";
+import { IncomeType, ItemType } from "../redux/addSchedule/slice";
+import dayjs from "dayjs";
+
+//カレンダーの作成とカレンダーの日付と同じ日に金額を表示する処理
+export const setSchedules = (
+  calendar: CalendarType,
+  schedules: Array<ItemType>,
+  income: Array<IncomeType>
+) => {
+  // const formatedSchedule = schedules.map((d: ItemType) => formatSchedule(d));
+
+  return calendar.map((c) => ({
+    date: c,
+    schedules: schedules.filter((e) => isSameDay(e.date, c)),
+    income: income.filter((e) => isSameDay(e.date, c)),
+  }));
+};
+
+export const formatSchedule = (schedule: ItemType) => ({
+  ...schedule,
+  date: dayjs(schedule.date),
+});
