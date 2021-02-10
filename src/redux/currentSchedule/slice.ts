6384d2df89b885e68cb8952c5cd73b485fdff687
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2021-02-10 17:33:08 +0900

    収入追加

--- src/redux/currentSchedule/slice.ts
+++ src/redux/currentSchedule/slice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { ItemType } from "../addSchedule/slice";
+import { IncomeType, ItemType } from "../addSchedule/slice";
 import { RootState } from "../rootReducer";
 import dayjs from "dayjs";
 import "dayjs/locale/ja";
@@ -7,12 +7,16 @@ dayjs.locale("ja");
 
 export type CurrentSchduleType = {
   item: ItemType | null;
+  income: IncomeType | null;
   isDialogOpen: boolean;
+  isIncomeDialogOpen: boolean;
 };
 
 export const init: CurrentSchduleType = {
   item: null,
+  income: null,
   isDialogOpen: false,
+  isIncomeDialogOpen: false,
 };
 
 export const currentScheduleSlice = createSlice({
@@ -20,9 +24,10 @@ export const currentScheduleSlice = createSlice({
   initialState: init,
   reducers: {
     setCurrentSchedule: (state, action: PayloadAction<ItemType>) => {
-      console.log("Date", action.payload.date);
-
       state.item = action.payload;
+    },
+    setCurrentIncome: (state, actions: PayloadAction<IncomeType>) => {
+      state.income = actions.payload;
     },
     setOpenCurrentDialog: (state) => {
       state.isDialogOpen = true;
@@ -30,11 +35,20 @@ export const currentScheduleSlice = createSlice({
     setCloseCurrentDialog: (state) => {
       state.isDialogOpen = false;
     },
+    setOpenCurrentIncomeDialog: (state) => {
+      state.isIncomeDialogOpen = true;
+    },
+    setCloseCurrentIncomeDialog: (state) => {
+      state.isIncomeDialogOpen = false;
+    },
   },
 });
 
 export const selectCurrentSchedule = (state: RootState) => state.current.item;
+export const selectCurrentIncome = (state: RootState) => state.current.income;
 export const selectCurrentDialogStatus = (state: RootState) =>
   state.current.isDialogOpen;
+export const selectCurrentIncomeDialogStatus = (state: RootState) =>
+  state.current.isIncomeDialogOpen;
 
 export default currentScheduleSlice;
