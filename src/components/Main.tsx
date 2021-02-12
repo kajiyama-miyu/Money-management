2595de63072981d80eb8ac5be4bdb326265b1742
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2021-02-12 14:14:15 +0900

    タブ切り替え

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,16 +3,30 @@ import CalendarBoard from "../components/CalendarBoard";
 import Navigation from "../components/Navigation";
 import dayjs from "dayjs";
 import "dayjs/locale/ja";
+import HeaderMenu from "../components/Navigation/header";
 
 dayjs.locale("ja");
 
-const Main: React.FC = () => {
+export type Props = {
+  index: number;
+  value: number;
+};
+
+const TabPanel: React.FC<Props> = (props) => {
+  const { index, value, children } = props;
+  console.log(index);
+  console.log(value);
+  console.log(children);
   return (
-    <div>
-      <Navigation />
-      <CalendarBoard />
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+    >
+      {value === index && children}
     </div>
   );
 };
 
-export default Main;
+export default TabPanel;
