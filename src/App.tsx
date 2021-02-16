43483defcc133b41b97ab6fbf1ff4010d1e57699
澤口ありさ <sawaguchiarisa@sawaguchiarisanoMacBook-Pro.local>
2021-02-16 10:06:23 +0900

    Appビビ修正

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import PiePlot from "./components/PiePlot";
 import Main from "./components/Main";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import MoneyData from "./components/MoneyData/MoneyData";
 const App: React.FC = () => {
   return (
     <Router>
       <Switch>
         <Route path="/pieprot" component={PiePlot} exact />
         <Route path="/" component={Main} exact />
+        <Route path="/moneydata" component={MoneyData} exact></Route>
       </Switch>
     </Router>
   );
