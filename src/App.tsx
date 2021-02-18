767cbf784bf0a2fe4fdc9d72ed6460c0a5055ecb
澤口ありさ <sawaguchiarisa@sawaguchiarisanoMacBook-Pro.local>
2021-02-18 10:26:55 +0900

    微々修正

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import HeaderMenu from "./components/Navigation/header";
 import CalendarBoad from "./components/CalendarBoard";
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import MoneyData from "./components/MoneyData/MoneyData";
-
 const App: React.FC = () => {
   const [value, setValue] = useState(0);
 
@@ -18,13 +17,6 @@ const App: React.FC = () => {
   );
 
   return (
-    // <Router>
-    //   <Switch>
-    //     <Route path="/pieprot" component={PiePlot} exact />
-    //     <Route path="/" component={Main} exact />
-    //     <Route path="/moneydata" component={MoneyData} exact></Route>
-    //   </Switch>
-    // </Router>
     <div>
       <HeaderMenu onChengeTab={handleChangeComponent} value={value} />
       <TabPanel value={value} index={0}>
