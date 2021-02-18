926c33fe31b948d640bfcae7e6410d4062933b13
澤口ありさ <sawaguchiarisa@sawaguchiarisanoMacBook-Pro.local>
2021-02-18 15:02:17 +0900

    微々修正

--- src/components/MoneyData/MoneyData.tsx
+++ src/components/MoneyData/MoneyData.tsx
@@ -1,35 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 // import { selectMoneyData } from "../../store/moneyDataSlice";
 import { fetchIncomeData, fetchMoneyData } from "../../store/moneyDataSlice";
 import dayjs from "dayjs";
 import Month from "../Month";
 import SwitchButton from "./SwitchButton";
+import { AuthContext } from "../../auth/AuthProvider";
 // type MoneyInfo = { amount: number; jenre: string; date: Date; details: string };
 
 const MoneyData: React.FC = () => {
   // const moneyData = useSelector(selectMoneyData);
-  const [userNum, setUserNum] = useState("abcde");
+
   const [year, setYear] = useState(dayjs().year());
   const [month, setMonth] = useState(dayjs().month() + 1);
+  const { uid } = useContext(AuthContext);
+
   const [arg, setArg] = useState<{
     userNum: string;
     year: number;
     month: number;
-  }>({ userNum: userNum, year: year, month: month });
+  }>({ userNum: uid, year: year, month: month });
 
   const onClickPreviousMonth = () => {
     const day = dayjs(`${year}-${month}`).add(-1, "month");
     setYear(day.year());
     setMonth(day.month() + 1);
-    setArg({ userNum: userNum, year: day.year(), month: day.month() + 1 });
+    setArg({ userNum: uid, year: day.year(), month: day.month() + 1 });
   };
 
   const onClickNextMonth = () => {
     const day = dayjs(`${year}-${month}`).add(1, "month");
     setYear(day.year());
     setMonth(day.month() + 1);
-    setArg({ userNum: userNum, year: day.year(), month: day.month() + 1 });
+    setArg({ userNum: uid, year: day.year(), month: day.month() + 1 });
   };
 
   const dispatch = useDispatch();
