90ab8d2b39f57f0f34fa77c20d551c359fdfab70
澤口ありさ <sawaguchiarisa@sawaguchiarisanoMacBook-Pro.local>
2021-02-18 10:55:44 +0900

    微々修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,13 @@
-import React, { useState, useCallback } from "react";
+import React from "react";
 import "./App.css";
-import PiePlot from "./components/PiePlot";
-import TabPanel from "./components/Main";
-import HeaderMenu from "./components/Navigation/header";
-import CalendarBoad from "./components/CalendarBoard";
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import MoneyData from "./components/MoneyData/MoneyData";
 import PrivateRoute from "./auth/PrivateRoute";
 import { AuthProvider } from "./auth/AuthProvider";
 import Login from "./auth/Login";
-import { Route, Switch, Router, BrowserRouter } from "react-router-dom";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
 
 const App: React.FC = () => {
   return (
-    <div>
-      <HeaderMenu onChengeTab={handleChangeComponent} value={value} />
-      <TabPanel value={value} index={0}>
-        <CalendarBoad />
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <PiePlot />
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        <MoneyData />
-      </TabPanel>
-    </div>
     <BrowserRouter>
       <AuthProvider>
         {/* <Router> */}
