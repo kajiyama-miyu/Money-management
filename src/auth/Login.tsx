6a1129671ab9d678174a48dc4c6210d3ecec82ca
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2021-02-18 10:21:17 +0900

    権限の切り替え

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -1,26 +1,55 @@
-import React from "react";
-import { Button, Card, CardActions } from "@material-ui/core";
+import React, { useContext } from "react";
+import { Button, Card, CardActions, CardHeader, Grid } from "@material-ui/core";
+import { AuthContext } from "./AuthProvider";
 
+const styles: { [key: string]: React.CSSProperties } = {
+  container: {
+    display: "flex",
+    flexWrap: "wrap",
+    width: "400",
+    marginTop: "50px",
+  },
+  header: {
+    textAlign: "center",
+    background: "#212121",
+    color: "#fff",
+  },
+  card: {
+    marginTop: "10px",
+  },
+  loginBtn: {
+    backgroundColor: "#87CEFA",
+  },
+};
 const Login: React.FC = () => {
-  const styles: { [key: string]: React.CSSProperties } = {
-    container: {
-      height: "90vh",
-    },
-    grid: {
-      borderLeft: "1px solid #ccc",
-      borderTop: "1px solid #ccc",
-    },
-    days: {
-      borderRight: "1px solid #ccc",
-      paddingTop: "10px",
-    },
+  const { login } = useContext(AuthContext);
+
+  const handleSubmit = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    e.preventDefault();
+    login();
   };
 
   return (
-    <Card>
-      <CardActions>
-        <Button></Button>
-      </CardActions>
-    </Card>
+    <form style={styles.container}>
+      <Grid container alignItems="center" justify="center">
+        <Grid item>
+          <Card style={styles.card}>
+            <CardHeader style={styles.header} title="ログイン" />
+            <CardActions>
+              <Button
+                variant="contained"
+                size="large"
+                style={styles.loginBtn}
+                onClick={(e) => handleSubmit(e)}
+              >
+                Googleアカウントでログイン
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+    </form>
   );
 };
+
+export default Login;
